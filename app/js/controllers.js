9e22a5da8b601a8d045edbd7ec258c8fd23cc632
Paula Rodriguez <paula.rp93@gmail.com>
2015-08-04 22:52:20 +0200

    Chapter 32 completed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,28 +6,21 @@ angular.module('myApp.controllers', [])
 .controller('LandingPageController', [function() {
 	
   }])
-.controller('WaitlistController', ['$scope', '$firebase', 'FIREBASE_URL', function($scope, $firebase, FIREBASE_URL) {
-	var partiesRef = new Firebase(FIREBASE_URL + 'parties');
-
-	$scope.parties = $firebase(partiesRef);
-	$scope.party = {name: '', phone: '', size: '', done: false, notified: 'No'};
+.controller('WaitlistController', ['$scope', 'partyService', 'textMessageService', function($scope, partyService, textMessageService) {
+	//Bind firebase parties to $scope
+	$scope.parties = partyService.parties;
+	
+	//Object to store data from the waitlist form
+	$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
+	
 	//Function to save a new party to the waitlist
 	$scope.saveParty = function(){
-		$scope.parties.$add($scope.newParty);
-		$scope.newParty = {name: '', phone: '', size: '', done:false, notified: 'No'};
+		partyService.saveParty($scope.newParty);
+		$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
 	};
 	//Function to send a text message to a party
 	$scope.sendTextMessage = function(party){
-		var textMessageRef = new Firebase(FIREBASE_URL + 'textMessages');
-		var textMessages = $firebase(textMessageRef);
-		var newTextMessage = {
-			phoneNumber: party.phone,
-			size : party.size,
-			name: party.name
-		}
-		textMessages.$add(newTextMessage);
-		party.notified = 'Yes';
-		$scope.parties.$save(party.$id);
+		textMessageService.sendTextMessage(party);
 	};
   }])
 .controller('AuthController', ['$scope', 'authService', function($scope, authService){
--- app/js/services.js
+++ app/js/services.js
@@ -7,6 +7,54 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
 .value('FIREBASE_URL', 'https://waitandeat-paula.firebaseio.com/')
+//This factory is used to refactor the code to create a firebase. Then, this 
+//service is injected to the other services
+.factory('dataService', function($firebase, FIREBASE_URL){
+	var dataRef = new Firebase(FIREBASE_URL);
+	var fireData = $firebase(dataRef);
+
+	return fireData;
+})
+.factory('partyService', function(dataService){
+	//Before
+	//var partiesRef = new Firebase(FIREBASE_URL + 'parties');
+	//var parties = $firebase(partiesRef);
+
+	//After refactoring with dataService
+	var parties = dataService.$child('parties');
+
+	var partyServiceObject = {
+		parties: parties, 
+		saveParty: function(party){
+			parties.$add(party);
+		}
+	};
+	return partyServiceObject;
+})
+.factory('textMessageService', function(dataService, partyService){
+	//Before
+	//var textMessageRef = new Firebase(FIREBASE_URL + 'textMessages');
+	//var textMessages = $firebase(textMessageRef);
+	
+	//After refactoring with dataService
+	var textMessages = dataService.$child('textMessages');
+
+	var textMessageServiceObject = {
+		sendTextMessage : function(party){
+			var newTextMessage = {
+				phoneNumber: party.phone,
+				size : party.size,
+				name: party.name
+			};
+			textMessages.$add(newTextMessage);
+			party.notified = 'Yes';
+			//$scope.parties.$save(party.$id);	
+			//We need inject partyService to allow textMessageService save parties.
+			partyService.parties.$save(party.$id);
+		}
+	};
+	return textMessageServiceObject;
+})
 .factory('authService', function($firebaseSimpleLogin, $location, $rootScope, FIREBASE_URL){
 	var authRef = new Firebase(FIREBASE_URL);
 	var auth = $firebaseSimpleLogin(authRef);
